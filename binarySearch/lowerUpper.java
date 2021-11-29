093a6b11a5c01fd66e3c66038eaa584ab363ce5b
mayu_rv <mayur99rv@users.noreply.github.com>
2021-11-29 22:29:56 +0530

    lower bound and upper bound of an ele in array

--- binarySearch/lowerUpper.java
+++ binarySearch/lowerUpper.java
@@ -17,7 +17,7 @@ public class lowerUpper {
         return ans;
     }
 
-    public static int upperBound(int[] a, int x) {
+    public static int justLower(int[] a, int x) {
         // first element which is just less than x
         int lo = 0;
         int hi = a.length - 1;
@@ -35,15 +35,38 @@ public class lowerUpper {
         return ans;
     }
 
+    public static int upperBound(int[] a, int x) {
+        // first element found is greater than x
+        int lo = 0;
+        int hi = a.length - 1;
+        int ans = a.length;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (a[mid] <= x) {
+                lo = mid + 1;
+            } else if (a[mid] > x) {
+                ans = mid;
+                hi = mid - 1;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 6, 7, 9, 10 };
-        int key = 5;
+        int key = 6;
         int index = lowerBound(arr, key);
+        int jindex = justLower(arr, key);
         int uindex = upperBound(arr, key);
         if (index == arr.length) {
             System.out.println("Not found");
         } else {
             System.out.println("lower Bound: index:" + index + ", val: " + arr[index]);
+        }
+        if (jindex == arr.length) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Just Lower: index:" + jindex + ", val: " + arr[jindex]);
         }
         if (uindex == arr.length) {
             System.out.println("Not found");
