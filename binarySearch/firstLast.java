1504f204a5a9c04e2e3aeab1aa83bd9d9a605638
mayu_rv <mayur99rv@users.noreply.github.com>
2021-11-29 21:31:57 +0530

    last and first ocurrence of duplicate

---
+++ binarySearch/firstLast.java
@@ -0,0 +1,69 @@
+public class firstLast {
+    private static int firstOccurence(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length - 1;
+        int first = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < arr[mid]) {
+                hi = mid - 1;
+            } else if (key > arr[mid]) {
+                lo = mid + 1;
+            } else {
+                if (first == -1) {
+                    first = mid;
+                } else if (mid < first) {
+                    first = mid;
+                }
+                hi = mid - 1;
+            }
+
+        }
+        if (first == -1) {
+            return -1;
+        } else
+            return first;
+
+    }
+
+    private static int lastOccurence(int[] arr, int key) {
+        int lo = 0;
+        int hi = arr.length - 1;
+        int last = -1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < arr[mid]) {
+                hi = mid - 1;
+            } else if (key > arr[mid]) {
+                lo = mid + 1;
+            } else {
+                if (last == -1) {
+                    last = mid;
+                } else if (mid > last) {
+                    last = mid;
+                }
+                lo = mid + 1;
+            }
+
+        }
+        if (last == -1) {
+            return -1;
+        } else
+            return last;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8 };
+        int key = 4;
+        int findex = firstOccurence(arr, key);
+        int lindex = lastOccurence(arr, key);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("first index at : " + findex + "\n last index at : " + lindex);
+        // first index at : 4
+        // last index at : 10
+    }
+}
